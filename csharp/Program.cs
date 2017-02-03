a3e507006115dad86ea98bd9e79f5d2fea71d2c7
Max Mahnik <maxmahnik@gmail.com>
2017-02-03 19:27:08 +0200

    Commit

---
+++ csharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    public class Program
+    {
+        static void Main(string[] args){
+
+            WideningCasting wideningCasting = new WideningCasting();
+            wideningCasting.Conversion();
+            Console.ReadLine();
+
+            NarrowingCasting narrowingCasting = new NarrowingCasting();
+            narrowingCasting.Narrowing();
+            Console.ReadLine();        
+        }
+    }
+}
