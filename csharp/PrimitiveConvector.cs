0a948cc5923d3996fea6bda4127511abb4bb96bc
Max Mahnik <maxmahnik@gmail.com>
2017-02-04 13:26:07 +0200

    Recover part1

---
+++ csharp/PrimitiveConvector.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    class PrimitiveConvector
+    {
+
+        public char floatToChar(float myFloat)
+        {
+            char myChar = '!';
+            myChar = (char)myFloat;
+            return myChar;
+        }
+
+        public char intToChar(int myInt)
+        {
+            char myChar = '^';
+            myChar = (char)myInt;
+            return myChar;
+
+        }
+
+        public int charToInt(char myChar)
+        {
+            int myInt = 1488;
+            myInt = (int)myChar;
+            return myInt;
+        }
+
+        public int doubleToInt(double myDouble)
+        {
+            int myInt = 9090;
+            myInt = (int)myDouble;
+            return myInt;
+        }
+    }
+}
---
+++ csharp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WideningCasting wideningCasting = new WideningCasting();
+            wideningCasting.Conversion();
+            Console.ReadLine();
+
+            NarrowingCasting narrowingCasting = new NarrowingCasting();
+            narrowingCasting.Narrowing();
+            Console.ReadLine();
+
+            //Homework Lesson 2
+            PrimitiveConvector convector = new PrimitiveConvector();
+
+            float defaultFloat = 3.1489F;
+            char charOutput = convector.floatToChar(defaultFloat);
+            Console.WriteLine("Input float value is " + defaultFloat
+            + ". Output char value is " + charOutput + ". Type of variable is - " + charOutput.GetType());
+            Console.ReadLine();
+
+            int defaultInt = 64;
+            char charOutput2 = convector.intToChar(defaultInt);
+            Console.WriteLine("Input int value is " + defaultInt
+            + ". Output char value is " + charOutput2 + ". Type of variable is - " + charOutput2.GetType());
+            Console.ReadLine();
+
+            char defaultChar = '@';
+            int intOutput = convector.charToInt(defaultChar);
+            Console.WriteLine("Input char value is " + defaultChar
+            + ". Output int value is " + intOutput + ". Type of variable is - " + intOutput.GetType());
+            Console.ReadLine();
+
+            double defaultDouble = 1.7976931348623158E+308;
+            int intOutput2 = convector.doubleToInt(defaultDouble);
+            Console.WriteLine("Input double value is " + defaultDouble
+            + ". Output int value is " + intOutput2 + ". Type of variable is - " + intOutput2.GetType());
+            Console.ReadLine();
+        }
+    }
+}
