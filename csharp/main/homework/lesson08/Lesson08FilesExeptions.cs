67a6079573613c221ea383c4634cfd66340146bf
Max Mahnik <maxmahnik@gmail.com>
2017-03-29 22:27:26 +0300

    08 start

---
+++ csharp/main/homework/lesson08/Lesson08FilesExeptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp.main.homework.lesson08
+{
+    class Lesson08FilesExeptions
+    {
+    }
+}
